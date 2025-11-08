20cb4690a2a86ca5635ff634b296253ec972c75c
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2025-11-08 19:08:18 +0100

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Terminate already running bar instances
 killall -q polybar
-
-# Wait until processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch bar
 polybar main &
