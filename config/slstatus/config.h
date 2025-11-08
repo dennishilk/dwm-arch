90014fa6a807c67078956cf4697d03286bd97a3b
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2025-11-08 12:01:35 +0100

    Create config.h

---
+++ config/slstatus/config.h
@@ -0,0 +1,7 @@
+static const struct arg args[] = {
+    { cpu_perc,     "CPU %s%% | ", NULL },
+    { ram_perc,     "RAM %s%% | ", NULL },
+    { disk_perc,    "Disk %s%% | ", "/" },
+    { vol_perc,     "Vol %s%% | ", "Master" },
+    { datetime,     "%s",          "%F %R" },
+};
