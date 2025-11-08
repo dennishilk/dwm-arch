8dab22a07d516f44f8acd98b0d8a5146c2c1ed39
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2025-11-08 15:19:53 +0100

    Update build.sh

--- ~/.config/suckless/slstatus/build.sh
+++ ~/.config/suckless/slstatus/build.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+set -e
 
+echo "📊 Building slstatus..."
+cd "$(dirname "$0")"
+rm -rf slstatus
+git clone https://git.suckless.org/slstatus
+cp config.h slstatus
+cd slstatus
+sudo make clean install
+echo "✅ slstatus installed successfully."
