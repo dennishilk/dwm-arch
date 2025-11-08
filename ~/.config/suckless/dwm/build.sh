49f923c0471adce8d1947548d12b8464d75b2959
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2025-11-08 15:18:49 +0100

    Update build.sh

--- ~/.config/suckless/dwm/build.sh
+++ ~/.config/suckless/dwm/build.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
+set -e
 
+echo "🧱 Building DWM..."
+cd "$(dirname "$0")"
+rm -rf dwm-6.4
+curl -LO https://dl.suckless.org/dwm/dwm-6.4.tar.gz
+tar -xzf dwm-6.4.tar.gz
+cp config.h dwm-6.4
+cd dwm-6.4
+sudo make clean install
+echo "✅ DWM installed successfully."
