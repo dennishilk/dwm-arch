36384a70b84c5f0c58d67fa37eb5c0c761b06b3f
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2025-11-08 14:29:31 +0100

    Update config.h

--- config/slstatus/config.h
+++ config/slstatus/config.h
@@ -1,7 +1,13 @@
+/* See LICENSE file for copyright and license details. */
+
+const unsigned int interval = 1000;
+static const char unknown_str[] = "n/a";
+#define MAXLEN 2048
+
 static const struct arg args[] = {
-    { cpu_perc,     "CPU %s%% | ", NULL },
-    { ram_perc,     "RAM %s%% | ", NULL },
-    { disk_perc,    "Disk %s%% | ", "/" },
-    { vol_perc,     "Vol %s%% | ", "Master" },
-    { datetime,     "%s",          "%F %R" },
+	{ cpu_perc,   "CPU %s%% | ",   NULL },
+	{ ram_perc,   "RAM %s%% | ",   NULL },
+	{ disk_perc,  "SSD %s%% | ",   "/" },
+	{ vol_perc,   "Vol %s%% | ",   "Master" },
+	{ datetime,   "%s",            "%F %R" },
 };
